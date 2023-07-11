ef10217290f8fea8ec5735367effac6e8ccd284f
Amal Ragh K <amalraghk123@gmail.com>
2023-07-11 13:40:18 +0530

    cretated the basic css

---
+++ index.js
@@ -0,0 +1,6 @@
+const container=document.querySelector('.conatiner');
+const menuu=document.querySelector('.menu');
+const eraser=document.querySelector('.eraser');
+const colorPick=document.querySelector('.colorPick');
+const size=document.querySelector('.size');
+const grid=document.querySelector('.grid');
